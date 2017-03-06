eec96560977333eb20707bae56f523c6623345f8
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-03-06 08:16:34 -0600

    Count newlines inside multiline comments

--- lexer_parser.py
+++ lexer_parser.py
@@ -23,12 +23,17 @@ states = (
     ('MULTILINECOMMENT', 'exclusive'),
 )
 
-t_MULTILINECOMMENT_ignore = ' \t\n'
+t_MULTILINECOMMENT_ignore = ' \t'
 
 
 def t_MULTILINECOMMENT(t):
     r'/\*'
     t.lexer.begin('MULTILINECOMMENT')
+
+
+def t_MULTILINECOMMENT_newline(t):
+    r'\n'
+    t.lexer.lineno += 1
 
 
 def t_MULTILINECOMMENT_close(t):
