902ca14577064e3be689c5d8d4638d8333b4db6c
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-03-08 14:24:33 -0600

    Revert parser to previous state before unwanted commit

--- parser.py
+++ parser.py
@@ -8,7 +8,8 @@ def p_empty(t):
 
 
 def p_program(t):
-    ''' program : PROGRAM ID ';' variable_declaration function_declaration block '''
+    ''' program : PROGRAM ID ; variable_declaration function_declaration 
+    block '''
     pass
 
 
@@ -18,8 +19,7 @@ def p_variable_declaration(t):
 
 
 def p_other_variable_declarations(t):
-    ''' other_variable_declarations : variable_declaration 
-                                    | empty '''
+    ''' other_variable_declarations : variable_declaration | empty '''
     pass
 
 
@@ -34,37 +34,12 @@ def p_ids(t):
     
 
 def p_other_ids(t):
-    ''' other_ids : ',' ids 
-                  | empty '''
+    ''' other_ids : ',' ids | empty '''
     pass
 
 
 def p_id(t):
-    ''' id : ID 
-           | ID '[' expression ']' '''
-
-
-def p_expression(t):
-    ''' expression : grouping
-                   | const
-                   | call '''
-
-
-def p_grouping(t):
-    ''' grouping : level1 
-                 | level1 EXPONENTIATION level1 '''
-
-
-def p_level1(t):
-    ''' level1 : level2 
-               | '+' level2 
-               | '-' level2 '''
-
-
-def p_level2(t):
-    ''' level2 : level3 
-               | level3 OR level3 
-               | level3 AND level3 '''
+    ''' id : ID | ID '[' const ']' '''
 
 
 def p_function_declaration(t):
@@ -73,63 +48,42 @@ def p_function_declaration(t):
 
 
 def p_other_function_declarations(t):
-    ''' other_function_declarations : function_declaration 
-                                    | empty '''
+    ''' other_function_declarations : function_declaration | empty '''
     pass
 
 
 def p_function(t):
-    '''function : FUN return_type ID '(' parameters ')' '{' variable_declaration statutes '}' ';' '''
+    '''function : FUN return_type ID '(' parameters ')' '{' function_variables
+    statutes '}' ';' '''
     pass
 
 
 def p_return_type(t):
-    ''' return_type : type 
-                    | VOID '''
+    ''' return_type : type | VOID '''
 
 
 def p_type(t):
-    ''' type : INT 
-             | DEC 
-             | CHAR 
-             | STR 
-             | BOOL '''
+    ''' type : INT | DEC | CHAR | STR | BOOL '''
     pass
 
 
 def p_statutes(t):
-    ''' statutes : call
-                 | assignment
-                 | condition
-                 | cycle 
-                 | special '''
-    pass
-
-
-def p_call(t):
-    ''' call : ID '(' expressions ')' '''
     pass
 
 
 def p_parameters(t):
-    ''' parameters : TYPE ID other_parameters'''
     pass
 
 
-def p_other_parameter(t):
-    ''' other_parameter : ',' parameters 
-                        | empty '''
+def p_function_variables(t):
     pass
+
 
 def p_const(t):
-    ''' const : id 
-              | INT 
-              | DEC '''
     pass
 
 
 def p_block(t):
-    ''' block : '{' statutes '}' '''
     pass
 
 
