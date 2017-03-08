a8b4a3886a226bc571877d6b9d2ad48a839421ed
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-03-08 17:39:04 -0600

    Add more parser grammar (work in progress)

--- parser.py
+++ parser.py
@@ -8,8 +8,7 @@ def p_empty(t):
 
 
 def p_program(t):
-    ''' program : PROGRAM ID ; variable_declaration function_declaration 
-    block '''
+    ''' program : PROGRAM ID ';' variable_declaration function_declaration block '''
     pass
 
 
@@ -19,7 +18,8 @@ def p_variable_declaration(t):
 
 
 def p_other_variable_declarations(t):
-    ''' other_variable_declarations : variable_declaration | empty '''
+    ''' other_variable_declarations : variable_declaration 
+                                    | empty '''
     pass
 
 
@@ -34,12 +34,68 @@ def p_ids(t):
     
 
 def p_other_ids(t):
-    ''' other_ids : ',' ids | empty '''
+    ''' other_ids : ',' ids 
+                  | empty '''
     pass
 
 
 def p_id(t):
-    ''' id : ID | ID '[' const ']' '''
+    ''' id : ID 
+           | ID '[' expression ']' '''
+
+
+def p_expression(t):
+    ''' expression : grouping
+                   | const
+                   | call '''
+
+
+def p_grouping(t):
+    ''' grouping : level1 
+                 | level1 EXPONENTIATION level1 '''
+
+
+def p_level1(t):
+    ''' level1 : level2 
+               | '+' level2 
+               | '-' level2 '''
+
+
+def p_level2(t):
+    ''' level2 : level3 
+               | level3 OR level3 
+               | level3 AND level3 '''
+
+
+def p_level3(t):
+    ''' level3 : level4
+               | level4 '<' level4
+               | level4 '>' level4
+               | level4 LESS_EQUAL_THAN level4
+               | level4 GREATER_EQUAL_THAN level4
+               | level4 EQUALS level4 '''
+
+
+def p_level4(t):
+    ''' level4 : level5
+               | level5 '+' level5
+               | level5 '-' level5 '''
+
+
+def p_level5(t):
+    ''' level5 : level6
+               | level6 '*' level6
+               | level6 '/' level6
+               | level6 MOD level6
+               |  '''
+
+
+def p_level6(t):
+    ''' level6 : '(' expression ')'
+               | const
+               | NOT const
+               | INCREMENT const
+               | DECREMENT const '''
 
 
 def p_function_declaration(t):
@@ -48,42 +104,93 @@ def p_function_declaration(t):
 
 
 def p_other_function_declarations(t):
-    ''' other_function_declarations : function_declaration | empty '''
+    ''' other_function_declarations : function_declaration 
+                                    | empty '''
     pass
 
 
 def p_function(t):
-    '''function : FUN return_type ID '(' parameters ')' '{' function_variables
-    statutes '}' ';' '''
+    '''function : FUN return_type ID '(' parameters ')' '{' variable_declaration statutes '}' ';' '''
     pass
 
 
 def p_return_type(t):
-    ''' return_type : type | VOID '''
+    ''' return_type : type 
+                    | VOID '''
 
 
 def p_type(t):
-    ''' type : INT | DEC | CHAR | STR | BOOL '''
+    ''' type : INT 
+             | DEC 
+             | CHAR 
+             | STR 
+             | BOOL '''
     pass
 
 
 def p_statutes(t):
+    ''' statutes : call
+                 | assignment
+                 | condition
+                 | cycle 
+                 | special '''
+    pass
+
+
+def p_call(t):
+    ''' call : ID '(' expressions ')' '''
+    pass
+
+
+def p_expressions(t):
+    ''' expressions : expression
+                    | expression ',' expressions '''
+
+
+def p_assignment(t):
+    ''' assignment : ID '=' expression ';'
+                   | ID '[' expression ']' '=' expression ';' '''
+    pass
+
+
+def p_condition(t):
+    ''' condition : IF '[' expression ']' block elses ';' '''
+
+
+def p_cycle(t):
+    ''' cycle : WHILE '(' expression ')' block ';' '''
+
+
+def p_special(t):
+    ''' special :  '''
+
+
+def p_elses(t):
+    ''' elses : empty
+              | ELSE block
+              | ELSEIF '(' expression ')' block elses '''
     pass
 
 
 def p_parameters(t):
+    ''' parameters : type ID other_parameters'''
     pass
 
 
-def p_function_variables(t):
+def p_other_parameters(t):
+    ''' other_parameters : ',' parameters 
+                         | empty '''
     pass
-
 
 def p_const(t):
+    ''' const : id 
+              | INT 
+              | DEC '''
     pass
 
 
 def p_block(t):
+    ''' block : '{' statutes '}' '''
     pass
 
 
