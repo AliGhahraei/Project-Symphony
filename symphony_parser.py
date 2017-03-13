6d33c35d97fb4fa8bebe286fd6b2669d4bee3a00
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-03-12 18:13:33 -0600

    Add global scope and replace name of ply's parameter from t to p

--- symphony_parser.py
+++ symphony_parser.py
@@ -1,62 +1,73 @@
+from lexer import tokens, Types
 from ply.yacc import yacc
-from lexer import tokens
 from sys import exit
 
 
-def p_program(t):
+functions_directory = {}
+global_scope = None
+
+
+class FunctionScope():
+    def __init__(self, name, return_type):
+        self.name = name
+        self.return_type = return_type
+        self.variables = {}
+
+
+def p_program(p):
     ''' program : PROGRAM ID ';' variable_declaration function_declaration block '''
-    pass
+    global_scope = FunctionScope(p[2], 'VOID')
 
 
-def p_empty(t):
+def p_empty(p):
     ''' empty : '''
     pass
 
 
-def p_variable_declaration(t):
+def p_variable_declaration(p):
     ''' variable_declaration : type ids ';' variable_declaration
                              | empty '''
     pass
 
 
-def p_ids(t):
+def p_ids(p):
     ''' ids : id other_ids '''
     pass
     
 
-def p_other_ids(t):
+def p_other_ids(p):
     ''' other_ids : ',' ids 
                   | empty '''
     pass
 
 
-def p_id(t):
+def p_id(p):
     ''' id : ID 
            | ID '[' expression ']' '''
     pass
 
 
-def p_expression(t):
+def p_expression(p):
     ''' expression : level1 
                    | level1 EXPONENTIATION level1  '''
     pass
 
 
-def p_level1(t):
+def p_level1(p):
     ''' level1 : level2 
                | '+' level2
                | '-' level2'''
     pass
 
 
-def p_level2(t):
+def p_level2(p):
     ''' level2 : level3 
                | level3 OR level3 
                | level3 AND level3 '''
     pass
 
 
-def p_level3(t):
+def p_level3(p):
     ''' level3 : level4
                | level4 '<' level4
                | level4 '>' level4
@@ -66,14 +77,14 @@ def p_level3(t):
     pass
 
 
-def p_level4(t):
+def p_level4(p):
     ''' level4 : level5
                | level5 '+' level5
                | level5 '-' level5 '''
     pass
 
 
-def p_level5(t):
+def p_level5(p):
     ''' level5 : level6
                | NOT level6
                | level6 '*' level6
@@ -82,7 +93,7 @@ def p_level5(t):
     pass
 
 
-def p_level6(t):
+def p_level6(p):
     ''' level6 : '(' expression ')'
                | const
                | increment
@@ -90,32 +101,32 @@ def p_level6(t):
     pass
 
 
-def p_increment(t):
+def p_increment(p):
     ''' increment : INCREMENT id '''
 
 
-def p_decrement(t):
+def p_decrement(p):
     ''' decrement : DECREMENT id '''
 
 
-def p_function_declaration(t):
+def p_function_declaration(p):
     ''' function_declaration : function function_declaration
                              | empty'''
     pass
 
 
-def p_function(t):
+def p_function(p):
     '''function : FUN return_type ID '(' parameters ')' '{' variable_declaration statutes '}' ';' '''
     pass
 
 
-def p_return_type(t):
+def p_return_type(p):
     ''' return_type : type 
                     | VOID '''
     pass
 
 
-def p_type(t):
+def p_type(p):
     ''' type : INT 
              | DEC 
              | CHAR 
@@ -124,12 +135,12 @@ def p_type(t):
     pass
 
 
-def p_statutes(t):
+def p_statutes(p):
     ''' statutes : statute ';' statutes
                  | empty'''
 
 
-def p_statute(t):
+def p_statute(p):
     '''statute   : call
                  | assignment
                  | condition
@@ -141,62 +152,62 @@ def p_statute(t):
     pass
 
 
-def p_call(t):
+def p_call(p):
     ''' call : ID '(' expressions ')' '''
     pass
 
 
-def p_expressions(t):
+def p_expressions(p):
     ''' expressions : expression
                     | expression ',' expressions '''
     pass
 
 
-def p_assignment(t):
+def p_assignment(p):
     ''' assignment : id '=' expression '''
     pass
 
 
-def p_condition(t):
+def p_condition(p):
     ''' condition : IF '(' expression ')' block elses '''
     pass
 
 
-def p_cycle(t):
+def p_cycle(p):
     ''' cycle : WHILE '(' expression ')' block '''
     pass
 
 
-def p_special(t):
+def p_special(p):
     ''' special : SPECIAL_ID '(' expressions ')' '''
     pass
 
 
-def p_return(t):
+def p_return(p):
     ''' return : RETURN expression
                | RETURN '''
 
 
-def p_elses(t):
+def p_elses(p):
     ''' elses : empty
               | ELSE block
               | ELSEIF '(' expression ')' block elses '''
     pass
 
 
-def p_parameters(t):
+def p_parameters(p):
     ''' parameters : type ID other_parameters
                    | empty '''
     pass
 
 
-def p_other_parameters(t):
+def p_other_parameters(p):
     ''' other_parameters : ',' type ID other_parameters
                          | empty '''
     pass
 
 
-def p_const(t):
+def p_const(p):
     ''' const : id 
               | call
               | special
@@ -208,12 +219,12 @@ def p_const(t):
     pass
 
 
-def p_block(t):
+def p_block(p):
     ''' block : '{' statutes '}' '''
     pass
 
 
-def p_error(t):
+def p_error(p):
     raise ParserException
 
 
@@ -222,14 +233,3 @@ parser = yacc()
 
 class ParserException(Exception):
     pass
-
-
-'''
-while True:
-    try:
-        input_string = input('Project Symphony > ')
-        parser.parse(input_string)
-    except(EOFError, KeyboardInterrupt):
-        print('Bye!')
-        exit(0)
-'''
