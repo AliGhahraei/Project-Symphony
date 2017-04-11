9a3995c132cef9eb413d1c33fc6b96584f4b3840
Ali Ghahraei Figueroa <aligf94@gmail.com>
2017-04-11 01:15:14 -0500

    Remove unnecessary iterators

--- orchestra.py
+++ orchestra.py
@@ -2,22 +2,20 @@ from collections import namedtuple
 from lexer import Types
 
 
-MEMORY_SECTORS = (
+MEMORY_SECTORS = [
     ('global_', 10_000),
     ('local', 50_000),
     ('temporal', 170_000),
     ('constant', 200_000),
     ('end', 240_000),
-)
-sector_iterator = iter(MEMORY_SECTORS[:-1])
+]
 ADDRESS_TUPLE = namedtuple('ADDRESSES', [address[0] for address
-                                         in sector_iterator])
+                                         in MEMORY_SECTORS[:-1]])
 
 def generate_memory_addresses():
-    sector_iterator = iter(MEMORY_SECTORS)
-    current_address = sector_iterator.__next__()[1]
+    current_address = MEMORY_SECTORS[0][1]
     addresses = []
-    for sector in sector_iterator:
+    for sector in MEMORY_SECTORS[1:]:
         next_address = sector[1]
         sector_size = next_address - current_address
 
